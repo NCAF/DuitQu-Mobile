ca879419b9b544514c0ca5a3bd86b3999c6083a8
NCAF <117642011+NCAF@users.noreply.github.com>
2025-01-05 10:34:59 +0700

    Update category.dart

--- frontend_app/lib/models/category.dart
+++ frontend_app/lib/models/category.dart
@@ -13,6 +13,14 @@ class Category {
     this.updatedAt,
   });
 
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Category && runtimeType == other.runtimeType && id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
+
   factory Category.fromJson(Map<String, dynamic> json) {
     return Category(
       id: json['id'],
